b2412e3b2732efa6b38cb34420d97085fb8b577e
hayeonk <31617695+hayeonk@users.noreply.github.com>
2019-05-02 12:33:26 +0900

    2019.05.01

--- lan.py
+++ lan.py
@@ -2,7 +2,7 @@ import sys
 from itertools import combinations
 rl = lambda: sys.stdin.readline().split()
 
-for _ in range(input()):
+for t in xrange(input()):
 	n, m = map(int, rl())
 	X = map(int, rl())
 	Y = map(int, rl())
@@ -11,23 +11,23 @@ for _ in range(input()):
 	for i, j in combinations(range(n), 2):
 		graph[i][j] = graph[j][i] = (X[i] - X[j]) ** 2 + (Y[i] - Y[j]) ** 2
 	
-	for i in range(m):
+	for _ in xrange(m):
 		a, b = map(int, rl())
-		graph[a][b] = graph[b][a] = 0.0
+		graph[a][b] = graph[b][a] = 0
 	
 	added = [False] * n
 	minWeight = [1e10] * n
 	ret = minWeight[0] = 0.0
 	
-	for it in range(n):
+	for _ in xrange(n):
 		u = -1
-		for v in range(n):
+		for v in xrange(n):
 			if not added[v] and (u == -1 or minWeight[u] > minWeight[v]):
 				u = v
-		ret += minWeight[u] ** 0.5
 		added[u] = True
+		ret += minWeight[u] ** 0.5
 		
-		for v in range(n):
+		for v in xrange(n):
 			if not added[v] and minWeight[v] > graph[u][v]:
 				minWeight[v] = graph[u][v]
 	print ret
